2a70ed03a2172eb31c5858e83f08548afd34efdd
vanheaven <vanheaven6@gmail.com>
2020-07-30 23:24:51 +0300

    AdD JOIN solutions

--- JOIN_01.sql
+++ JOIN_01.sql
@@ -1,5 +1,5 @@
 --++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
---       TUTORIAL
+--                                 TUTORIAL
 --++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 -- Soln 1
 -- show the matchid and player name for all goals scored by Germany. 
@@ -59,3 +59,62 @@ SELECT mdate,
     FROM game JOIN goal ON matchid = id 
       GROUP BY mdate, team1, team2, teamid 
         ORDER BY  mdate, matchid, team1, team2;
+
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+--                                 QUIZ
+--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- soln1
+-- You want to find the stadium where player 'Dimitris Salpingidis' scored. Select the JOIN condition to use:
+--          SELECTION:- game  JOIN goal ON (id=matchid)
+
+-- soln2
+-- You JOIN the tables goal and eteam in an SQL statement. Indicate the list of column names that may be used in the SELECT line:
+-- matchid, teamid, player, gtime, id, teamname, coach
+
+-- soln3
+-- Select the code which shows players, their team and the amount of goals they scored against Greece(GRE).
+-- SELECTION 👇:
+SELECT player, teamid, COUNT(*)
+  FROM game JOIN goal ON matchid = id
+ WHERE (team1 = "GRE" OR team2 = "GRE")
+   AND teamid != 'GRE'
+ GROUP BY player, teamid;
+
+-- soln4
+-- Select the result that would be obtained from this code:
+SELECT DISTINCT teamid, mdate
+  FROM goal JOIN game on (matchid=id)
+ WHERE mdate = '9 June 2012';
+--    RESULT
+-- DEN	9 June 2012
+-- GER	9 June 2012
+
+-- soln5
+-- Select the code which would show the player and their team for those who have scored against Poland(POL) in National Stadium, Warsaw.
+-- SELECTION 👇:
+SELECT DISTINCT player, teamid 
+   FROM game JOIN goal ON matchid = id 
+  WHERE stadium = 'National Stadium, Warsaw' 
+ AND (team1 = 'POL' OR team2 = 'POL')
+   AND teamid != 'POL';
+
+-- soln6
+-- Select the code which shows the player, their team and the time they scored, 
+-- for players who have played in Stadion Miejski (Wroclaw) but not against Italy(ITA).
+-- SELECTION 👇:
+SELECT DISTINCT player, teamid, gtime
+  FROM game JOIN goal ON matchid = id
+ WHERE stadium = 'Stadion Miejski (Wroclaw)'
+   AND (( teamid = team2 AND team1 != 'ITA') OR ( teamid = team1 AND team2 != 'ITA'))
+-- soln7
+--  Select the result that would be obtained from this code:
+SELECT teamname, COUNT(*)
+  FROM eteam JOIN goal ON teamid = id
+ GROUP BY teamname
+HAVING COUNT(*) < 3
+
+-- SELECTION 👇:
+-- Netherlands	2
+-- Poland	2
+-- Republic of Ireland	1
+-- Ukraine	2
