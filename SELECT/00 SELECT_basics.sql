8963433f1d886197993c4fe70cfec8a7dd043494
Mworekwa Ezekiel <vanheaven6@gmail.com>
2020-07-30 23:51:14 +0300

    Update 00 SELECT_basics.sql

--- SELECT/00 SELECT_basics.sql
+++ SELECT/00 SELECT_basics.sql
@@ -3,13 +3,16 @@
 --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 --Soln 1
 -- show the population of Germany
+-- QUERY 👇:
 SELECT population FROM world WHERE name = 'Germany';
 
 --Soln 2
 -- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'
+-- QUERY 👇:
 SELECT name, population FROM world WHERE name IN ( 'Sweden', 'Norway', 'Denmark');
 
 --Soln 3
+-- QUERY 👇:
 -- show the country and the area for countries with an area between 200,000 and 250,000.
 SELECT name, area FROM world WHERE area BETWEEN 200000 AND 250000;
 
