307d55d95328eccaa5a989ebe23e6b6011e2af57
vanheaven <vanheaven6@gmail.com>
2020-07-30 23:54:16 +0300

    Update Select in select

--- SELECT/003 SELECT_in_select.sql
+++ SELECT/003 SELECT_in_select.sql
@@ -3,7 +3,7 @@
 --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 -- Soln 1
 -- List each country name where the population is larger than that of 'Russia'.
--- SELECTION 👇:
+-- QUERY 👇:
 SELECT name FROM world
   WHERE population >
      (SELECT population FROM world
@@ -11,32 +11,32 @@ SELECT name FROM world
 
 -- Soln 2
 -- Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
--- SELECTION 👇:
+-- QUERY 👇:
 SELECT name FROM world WHERE continent = 'Europe' AND gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom');
 
 -- Soln 3
 -- List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
--- SELECTION 👇:
+-- QUERY 👇:
 SELECT name, continent FROM world WHERE continent = (SELECT continent FROM world WHERE name = 'Argentina') OR continent = (SELECT continent FROM world WHERE name = 'Australia') ORDER BY name;
 
 -- Soln 4
 -- Which country has a population that is more than Canada but less than Poland? Show the name and the population.
--- SELECTION 👇:
+-- QUERY 👇:
 SELECT name, population FROM world WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland');
 
 -- Soln 5
 -- Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
--- SELECTION 👇:
+-- QUERY 👇:
 SELECT name, CONCAT(ROUND(100 * population/(SELECT population FROM world WHERE name = 'GErmany'), 0), '%') AS percentage FROM world WHERE continent = 'Europe';
 
 -- Soln 6
 -- Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
--- SELECTION 👇:
+-- QUERY 👇:
 SELECT name FROM world WHERE gdp > ALL(SELECT gdp FROM world WHERE continent = 'EUROPE' AND gdp > 0);
 
 -- Soln 7
 -- Find the largest country (by area) in each continent, show the continent, the name and the area:
--- SELECTION 👇:
+-- QUERY 👇:
 SELECT continent, name, area FROM world x
   WHERE area >= ALL
     (SELECT area FROM world y
@@ -45,18 +45,18 @@ SELECT continent, name, area FROM world x
 
 -- Soln 8
 -- List each continent and the name of the country that comes first alphabetically.
--- SELECTION 👇:
+-- QUERY 👇:
 SELECT continent, name AS first_country FROM world x WHERE name = (SELECT name FROM world y WHERE x.continent = y.continent ORDER BY name LIMIT 1);
 
 -- Soln 9
 -- Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents.
 -- Show name, continent and population.
--- SELECTION 👇:
+-- QUERY 👇:
 SELECT name, continent, population FROM world x WHERE 25000000 >= ALL(SELECT population FROM world y WHERE x.continent = y.continent);
 
 -- Soln 10
 -- Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
--- SELECTION 👇:
+-- QUERY 👇:
 SELECT name, continent FROM world x WHERE population/3 > ALL(SELECT population FROM world y WHERE x.continent = y.continent AND population > 0 AND NOT x.population = y.population);
 
 --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
